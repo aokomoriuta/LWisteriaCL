466e5aa475ea620aecf6bc78dff74f6fac383951
aokomoriuta_lab <internal@enmps.net>
2012-04-13 13:56:22 +0900

    Hello, world!

---
+++ LWisteriaCL/LWisteriaCLTest/LWisteriaCLTest.cs
@@ -0,0 +1,22 @@
+﻿namespace LWisteria.CL.Test
+{
+	/// <summary>
+	/// テストクラス
+	/// </summary>
+	static class TestMain
+	{
+		/// <summary>
+		/// エントリポイント
+		/// </summary>
+		/// <returns>終了コード</returns>
+		static int Main()
+		{
+			// はろー！
+			System.Console.WriteLine("Hello, world!");
+
+			// 入力待ち待機と終了
+			System.Console.ReadKey();
+			return 1;
+		}
+	}
+}
